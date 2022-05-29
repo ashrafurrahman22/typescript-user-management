9e376207a581d7566f6b6dcbe1f38058c02e5700
ashrafur22 <ashrafurrahman22@gmail.com>
2022-05-29 19:21:11 +0600

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/Counter';
+import Users from './Components/Users';
+
+let student : string = 'opu';
+let age : number = 22;
+let isSmart : boolean = true;
+
+let students : string[] = ['opu', 'dipu', 'tithy'];
+let fees : number[] = [ 23, 43, 54];
+
+interface Person {
+  name : string,
+  job : string,
+  age : number,
+  location?: string | number
+}
+
+const person : Person = {
+    name : 'Bill Clinton',
+    job : 'Bekar',
+    age : 85,
+    location : 24
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Counter></Counter>
+        <Users></Users>
     </div>
   );
 }
